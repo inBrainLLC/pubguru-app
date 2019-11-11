7444b60e1a07f4d9fac72be54ff1c7c4ee044850
Sergey Shlykov <razzeond@gmail.com>
2019-11-11 16:32:56 +0700

    moved table

--- src/Components/DetailTable/DetailTable.jsx
+++ src/Components/DetailTable/DetailTable.jsx
@@ -2,9 +2,25 @@ import React, { Component } from "react";
 
 class DetailTable extends Component {
   render() {
+    let rows = this.props.data.map(d => {
+        return (
+          <tr key={d.date}>
+            <td>{d.totalPageView}</td>
+            <td>{d.estimatedEuRevinew}</td>
+            <td>{d.nonEuPageviews}</td>
+            <td>{d.euPageviews}</td>
+            <td>{d.priviouslyConsented}</td>
+            <td>{d.shownAndConsented}</td>
+            <td>{d.shownAndNonConsented}</td>
+            <td>{d.gdprPopupCtr}</td>
+          </tr>
+        );
+      });
     return (
         <div>
-            DetailTable
+            <table border="1">
+                <tbody>{rows}</tbody>
+             </table>
         </div>
     );
   }
