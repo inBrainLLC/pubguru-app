5415432388948f647d19208ed01cbaa8637af691
jahan19011noor <jahannoor19011@gmail.com>
2019-11-11 16:10:13 +0600

    commiting the less issue

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,39 +1,62 @@
 import React, { Component } from "react";
+import { Menu, Icon } from "antd";
 
-// get our fontawesome imports
-import { faCaretDown, faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const { SubMenu } = Menu;
 
 class Header extends Component {
+  state = {
+    current: "mail"
+  };
+
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
+
   render() {
     return (
-      <div>
-        <header>
-          <ul>
-            <li>
-              <span className="uniform-padding">
-                <FontAwesomeIcon icon={faBars} />
-              </span>
-            </li>
-            <li>
-              <span className="non-uniform-padding">
-                <h5>Publishers (207)</h5>
-                <h4>
-                  Allafrica <FontAwesomeIcon icon={faCaretDown} />
-                </h4>
-              </span>
-            </li>
-            <li>
-              <span className="non-uniform-padding">
-                <h5>Domains (32)</h5>
-                <h4>
-                  example_1.com <FontAwesomeIcon icon={faCaretDown} />
-                </h4>
-              </span>
-            </li>
-          </ul>
-        </header>
-      </div>
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="mail">
+          <Icon type="mail" />
+          Navigation One
+        </Menu.Item>
+        <Menu.Item key="app" disabled>
+          <Icon type="appstore" />
+          Navigation Two
+        </Menu.Item>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="setting" />
+              Navigation Three - Submenu
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="alipay">
+          <a
+            href="https://ant.design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Navigation Four - Link
+          </a>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
