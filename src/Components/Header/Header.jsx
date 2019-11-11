c6b36c9a2213926de4e593c066dad44cb2474e89
jahan19011noor <jahannoor19011@gmail.com>
2019-11-11 17:54:54 +0600

    commiting header without style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Menu, Icon } from "antd";
+import { Menu, Icon, Badge } from "antd";
 
 const { SubMenu } = Menu;
 
@@ -22,19 +22,12 @@ class Header extends Component {
         selectedKeys={[this.state.current]}
         mode="horizontal"
       >
-        <Menu.Item key="mail">
-          <Icon type="mail" />
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled>
-          <Icon type="appstore" />
-          Navigation Two
-        </Menu.Item>
         <SubMenu
           title={
             <span className="submenu-title-wrapper">
-              <Icon type="setting" />
-              Navigation Three - Submenu
+              PUBLISHERS (207)
+              <br />
+              Allafrica
             </span>
           }
         >
@@ -47,15 +40,49 @@ class Header extends Component {
             <Menu.Item key="setting:4">Option 4</Menu.Item>
           </Menu.ItemGroup>
         </SubMenu>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              DOMAINS (32)
+              <br />
+              example_1.com
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="mail">
+          <img className="ant-menu-item" src="../../assets/img/logo.png" />
         </Menu.Item>
+        <Menu.Item key="mail">
+          <Badge count={5}>
+            <a href="#" className="head-example" />
+            <Icon type="bell" />
+          </Badge>
+        </Menu.Item>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="appstore" />
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
       </Menu>
     );
   }
