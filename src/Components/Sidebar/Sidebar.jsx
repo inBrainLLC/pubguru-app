4705d3571295b15c582aecbabcd2e50b7f497618
salmakona <gugly2009@gmail.com>
2019-11-12 11:17:19 +0600

    update all

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -3,47 +3,76 @@ import React, { Component } from "react";
 // get our fontawesome imports
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Menu, Icon } from 'antd';
+const { SubMenu } = Menu;
+
 
 class SideBar extends Component {
+  handleClick = e => {
+    console.log('click ', e);
+  };
+
   render() {
     return (
-      <div className="navbar">
-        <ul>
-          <li>
-            <span className="uniform-padding">
-            <FontAwesomeIcon icon={['fas', 'check-box']} />
-              Dashboard
+      <Menu
+        onClick={this.handleClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+      >
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <Icon type="mail" />
+              <span>Navigation One</span>
             </span>
-          </li>
-          <li>
-            <span className="uniform-padding">
-              <FontAwesomeIcon icon={faBars} />
-              
-              Notification
+          }
+        >
+          <Menu.ItemGroup key="g1" title="Item 1">
+            <Menu.Item key="1">Option 1</Menu.Item>
+            <Menu.Item key="2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup key="g2" title="Item 2">
+            <Menu.Item key="3">Option 3</Menu.Item>
+            <Menu.Item key="4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="appstore" />
+              <span>Navigation Two</span>
             </span>
-          </li>
-          <li>
-            <span className="uniform-padding">
-              <FontAwesomeIcon icon={faBars} />
-              Analytics Report
+          }
+        >
+          <Menu.Item key="5">Option 5</Menu.Item>
+          <Menu.Item key="6">Option 6</Menu.Item>
+          <SubMenu key="sub3" title="Submenu">
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+        </SubMenu>
+        <SubMenu
+          key="sub4"
+          title={
+            <span>
+              <Icon type="setting" />
+              <span>Navigation Three</span>
             </span>
-          </li>
-          <li>
-            <span className="uniform-padding">
-              <FontAwesomeIcon icon={faBars} />
-              AdX Report
-            </span>
-          </li>
-          <li>
-            <span className="uniform-padding">
-              <FontAwesomeIcon icon={faBars} />
-              Ad Network Report
-            </span>
-          </li>
-        </ul>
-      </div>
+          }
+        >
+          <Menu.Item key="9">Option 9</Menu.Item>
+          <Menu.Item key="10">Option 10</Menu.Item>
+          <Menu.Item key="11">Option 11</Menu.Item>
+          <Menu.Item key="12">Option 12</Menu.Item>
+        </SubMenu>
+      </Menu>
     );
   }
+
 }
 
 // ReactDOM.render(<App />, document.getElementById("app"));
