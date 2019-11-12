36127a7f2c2772d084fb7aaf78e1e788d3f4531b
jahan19011noor <jahannoor19011@gmail.com>
2019-11-12 12:28:11 +0600

    commiting title component

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
+import { Menu, Dropdown, Icon } from "antd";
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.alipay.com/"
+      >
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.taobao.com/"
+      >
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3" disabled>
+      3rd menu item（disabled）
+    </Menu.Item>
+  </Menu>
+);
 
 class Title extends Component {
   render() {
     return (
-        <div>
-            Title
+      <div>
+        <div className="title-bar">
+          <h1>GDPR Report</h1>
+          <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" href="#">
+              Hover me <Icon type="down" />
+            </a>
+          </Dropdown>
         </div>
+        <hr />
+      </div>
+      // <PageHeader
+      //   style={{
+      //     border: "1px solid rgb(235, 237, 240)"
+      //   }}
+      //   onBack={() => null}
+      //   title="Title"
+      //   subTitle="This is a subtitle"
+      // />
     );
   }
 }
--- src/Pages/Report/Report.jsx
+++ src/Pages/Report/Report.jsx
@@ -1,29 +1,27 @@
 import React, { Component } from "react";
 
-import Title from '../../Components/Title/Title';
-import Toolbar from '../../Components/Toolbar/Toolbar';
-import Overview from '../../Components/Overview/Overview';
-import DataChart from '../../Components/DataChart/DataChart';
-import DetailTable from '../../Components/DetailTable/DetailTable';
+import Title from "../../Components/Title/Title";
+import Toolbar from "../../Components/Toolbar/Toolbar";
+import Overview from "../../Components/Overview/Overview";
+import DataChart from "../../Components/DataChart/DataChart";
+import DetailTable from "../../Components/DetailTable/DetailTable";
 
-import data from './demoData';
+import data from "./demoData";
 
 class Report extends Component {
   state = {
-    data,
-  }
-  
+    data
+  };
+
   render() {
     return (
-        <div>
-            <Title />
-            <Toolbar />
-            <Overview />
-            <DataChart />
-            <DetailTable
-              data={this.state.data}
-            />
-        </div>
+      <div className="report-content">
+        <Title />
+        <Toolbar />
+        <Overview />
+        <DataChart />
+        <DetailTable data={this.state.data} />
+      </div>
     );
   }
 }
