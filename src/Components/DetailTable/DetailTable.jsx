1686de16d4624b6711a7f00fb15db61f39b0d014
salmakona <gugly2009@gmail.com>
2019-11-17 12:18:13 +0600

    update data tables

--- src/Components/DetailTable/DetailTable.jsx
+++ src/Components/DetailTable/DetailTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { Card,Table } from "antd";
+import { Icon,Tooltip } from 'antd';
 
 import './DetailTable.less';
 
@@ -16,49 +17,106 @@ class DetailTable extends Component {
   render() {
       const columns = [
         {
-          title: 'Date',
+          title: <span> <span className="title">Date</span>
+            </span>,
           dataIndex: 'date',
           sorter: (a, b) => a.date.length - b.date.length,
-          sortDirections: ['descend'],
+          onFilter: (date, record) => record.date.indexOf(date) === 0,
+
         },
         {
-          title: 'Total Page View',
+          title: <span>
+              <span className="question-circle"> 
+              <Tooltip title="Estimated earnign from EU countires(From GAM)">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+
+              <span className="title">Total Page View</span>
+          </span>,
           dataIndex: 'totalPageView',
           defaultSortOrder: 'descend',
           sorter: (a, b) => a.totalPageView - b.totalPageView,
         },
         {
-          title: 'Estimated Eu Revinew',
+          title: <span>
+
+              <span className="question-circle"> 
+              <Tooltip title="Estimated Eu Revinew">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+              <span className="title">  Estimated Eu Revinew</span>
+          </span>,
           dataIndex: 'estimatedEuRevinew',
           sorter: (a, b) => a.estimatedEuRevinew.length - b.estimatedEuRevinew.length,
           sortDirections: ['descend', 'ascend'],
         },
         {
-          title: 'Non Eu Page views',
+          title: 
+          <span>
+                 <span className="question-circle"> 
+              <Tooltip title="Non Eu Page views">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+              <span className="title">Non Eu Page views</span>
+            </span>,
           dataIndex: 'nonEuPageviews',
           sorter: (a, b) => a.nonEuPageviews.length - b.nonEuPageviews.length,
           sortDirections: ['descend', 'ascend'],
         },
         {
-          title: 'Priviously Consented',
+          title: <span>
+                  <span className="question-circle"> 
+              <Tooltip title="Priviously  Consented">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+              <span className="title">Priviously  Consented</span>
+            </span>,
           dataIndex: 'priviouslyConsented',
           sorter: (a, b) => a.priviouslyConsented.length - b.priviouslyConsented.length,
           sortDirections: ['descend', 'ascend'],
         },
         {
-          title: 'Shown And Consented',
+          title: 
+          <span>
+              <span className="question-circle"> 
+              <Tooltip title="Shown  And Consented">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+              <span className="title"> Shown  And Consented</span>
+          </span>,
           dataIndex: 'shownAndConsented',
           sorter: (a, b) => a.shownAndConsented.length - b.shownAndConsented.length,
           sortDirections: ['descend', 'ascend'],
         },
         {
-          title: 'Shown And Non Consented',
+          title:
+          <span> 
+                  <span className="question-circle"> 
+              <Tooltip title="Shown And Non Consented">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+          <span className="title">Shown And Non Consented</span>
+          </span>,
           dataIndex: 'shownAndNonConsented',
           sorter: (a, b) => a.shownAndNonConsented.length - b.shownAndNonConsented.length,
           sortDirections: ['descend', 'ascend'],
         },
         {
-          title: 'Gdpr Popup Ctr',
+          title: 
+          <span>
+                  <span className="question-circle"> 
+              <Tooltip title="Gdpr Popup Ctr">
+              <Icon type="question-circle" /> 
+              </Tooltip>
+              </span> 
+          <span className="title">Gdpr Popup Ctr</span>
+          </span>,
           dataIndex: 'gdprPopupCtr',
           sorter: (a, b) => a.gdprPopupCtr.length - b.gdprPopupCtr.length,
           sortDirections: ['descend', 'ascend'],
@@ -66,12 +124,9 @@ class DetailTable extends Component {
       ];
       const data = this.getData();
       console.log(data);
-
-
-
-  function onChange( filters, sorter) {
-    console.log('params', sorter);
-  }
+    function onChange( filters, sorter) {
+      console.log('params', sorter);
+    }
   return(
     <Card style={{ margin: '40px 0', borderRadius: '5px'}} className="datatable">
         <Table columns={columns} dataSource={data} onChange={onChange} />
